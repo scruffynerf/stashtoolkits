43aa63da761602b191ea173e4fb61576dde74b6a
Scruffy Nerf <scruffynerf23@gmail.com>
2023-10-16 21:28:07 -0400

    Add DupeTitle v0.2

---
+++ UtilitiesToolKit/plugins/DupeTitle/dupetitle_defaults.py
@@ -0,0 +1,15 @@
+# defaults if any
+enabled = True
+titledupe = True
+stashiddupe = True
+urldupe = True
+oshashdupe = True
+durationdupe = True
+neardurationdupe = True
+dupetitle_tagname = "Duplicate Title"
+dupestashid_tagname = "Duplicate StashID"
+dupeurl_tagname = "Duplicate URL"
+dupeoshash_tagname = "Duplicate File Hash"
+dupeduration_tagname = "Duplicate Duration"
+neardupeduration_tagname = "Near Duplicate Duration"
+neardupeduration_diff = 3
