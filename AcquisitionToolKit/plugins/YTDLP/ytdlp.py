4510b7e1ba41784aa00427fe939a437886e9f565
Scruffy Nerf <scruffynerf23@gmail.com>
2023-10-17 20:59:24 -0400

    improved ytdlp - no longer bulk json but individual, handles json or text file urls, stores seen urls and error urls

--- AcquisitionToolKit/plugins/YTDLP/ytdlp.py
+++ AcquisitionToolKit/plugins/YTDLP/ytdlp.py
@@ -1,9 +1,6 @@
-import pathlib
-import re
+import os
 import sys
 import json
-import os
-import shutil
 
 # plugins don't start in the right directory, let's switch to the local directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -33,11 +30,14 @@ if not os.path.exists("config.py"):
 
 import config
 
-if not os.path.exists(config.urls_txt):
-   with open(config.urls_txt, 'w') as firstrun:
+if not os.path.exists(config.urls_file):
+   with open(config.urls_file, 'w') as firstrun:
      firstrun.write("")
 if not os.path.exists(config.grabbed_urls_txt):
    with open(config.grabbed_urls_txt, 'w') as firstrun:
+     firstrun.write("")
+if not os.path.exists(config.error_urls_txt):
+   with open(config.error_urls_txt, 'w') as firstrun:
      firstrun.write("")
 
 def main():
@@ -55,6 +55,7 @@ def run(json_input, output):
     PLUGIN_ARGS = False
     HOOKCONTEXT = False
 
+    global stash
     try:
         stash = StashInterface(json_input["server_connection"])
     except Exception:
@@ -66,7 +67,6 @@ def run(json_input, output):
         pass
     if PLUGIN_ARGS == "download":
             read_urls_and_download(stash)
-            stash.metadata_scan(paths=[config.download_dir])
             output["output"] = "ok"
             return
 
@@ -79,99 +79,119 @@ def run(json_input, output):
     log.debug("--Starting YTDLP Hooks --")
     sceneID = HOOKCONTEXT['id']
     scene = stash.find_scene(sceneID)
-    tag_scene(scene, stash)
+    tag_scene(scene)
     output["output"] = "ok"
     return
 
 
-def tag_scene(scene, stash):
-    downloaded = False
-    if not os.path.isfile(config.downloaded_json) and not os.path.isfile(config.downloaded_backup_json):
-       return
-    if not os.path.isfile(config.downloaded_json) and os.path.isfile(config.downloaded_backup_json):
-        shutil.copyfile(config.downloaded_backup_json, config.downloaded_json)
-    locations = scene.get("files")
-    for location in locations:
-        if config.download_dir in location['path']:
-           downloaded = True
-    if downloaded == False:
-       log.debug(f"Not a Download - skipping")
-       return
+def tag_scene(scene):
     if scene.get('urls') != []:
        log.debug(f"already has URL - skipping")
        return
-    with open(config.downloaded_json) as json_file:
-        data = json.load(json_file)
-        basename = scene.get('files')[0]['basename']
-        filename = os.path.splitext(basename)[0]
-        found_video = None
-        for video in data:
-            if video['id'] in filename:
-               found_video = video
-               break
-        if found_video is not None:
-                  ## currently optimized for PH, could be adjusted for other sites
-                  scene_data = {
-                    'ids': [scene.get('id')],
-                    'url': video['webpage_url'],
-                    'title': video['fulltitle']
-                  }
+    locations = scene.get("files")
+    jsonfile = ""
+    for location in locations:
+       path = location['path']
+       jsonpath = os.path.splitext(path)[0] + '.json'
+       if os.path.exists(jsonpath):
+          jsonfile = jsonpath
+          log.debug(jsonpath)
+    if jsonfile == "":
+       log.debug(f"No json found - skipping")
+       return
+    with open(jsonfile, 'r') as videoinfo:
+         video = json.load(videoinfo)
 
-                  tag_ids = []
-                  if video.get('tags') is not None:
-                    for tag in video.get('tags'):
-                        tag_id = stash.find_tag(tag, create=True)
-                        tag_ids.append(tag_id.get('id'))
-                  if video.get('categories') is not None:
-                    for tag in video.get('categories'):
-                        tag_id = stash.find_tag(tag, create=True)
-                        tag_ids.append(tag_id.get('id'))
-                  if tag_ids:
-                     scene_data['tag_ids'] = { "ids": tag_ids, "mode": "ADD" }
+    ## currently optimized for PH, could be adjusted for other sites' json
+    scene_data = {
+        'ids': [scene.get('id')],
+        'url': video['webpage_url'],
+        'title': video['fulltitle']
+    }
 
-                  performer_ids = []
-                  if video.get('cast') is not None:
-                    for performer in video.get('cast'):
-                        performer_ids.append(stash.find_performer(performer, create=True).get('id'))
-                    scene_data['performer_ids'] = { "ids": performer_ids, "mode": "ADD" }
+    tag_ids = []
+    if video.get('tags') is not None:
+        for tag in video.get('tags'):
+            tag_id = stash.find_tag(tag, create=True)
+            tag_ids.append(tag_id.get('id'))
+    if video.get('categories') is not None:
+        for tag in video.get('categories'):
+            tag_id = stash.find_tag(tag, create=True)
+            tag_ids.append(tag_id.get('id'))
+    if tag_ids:
+        scene_data['tag_ids'] = { "ids": tag_ids, "mode": "ADD" }
 
-                  if video.get('uploader') is not None:
-                    scene_data['studio_id'] = stash.find_studio(video.get('uploader'), create=True).get('id')
+    performer_ids = []
+    if video.get('cast') is not None:
+        for performer in video.get('cast'):
+            performer_ids.append(stash.find_performer(performer, create=True).get('id'))
+        scene_data['performer_ids'] = { "ids": performer_ids, "mode": "ADD" }
 
-                  if video.get('upload_date') is not None:
-                    scene_data['date'] = video.get('upload_date')[0:4] + "-" + video.get('upload_date')[4:6] + "-" + video.get('upload_date')[6:8]
-                    log.debug(scene_data['date'])
+    if video.get('uploader') is not None:
+        scene_data['studio_id'] = stash.find_studio(video.get('uploader'), create=True).get('id')
 
-                  stash.update_scenes(scene_data)
+    if video.get('upload_date') is not None:
+        scene_data['date'] = video.get('upload_date')[0:4] + "-" + video.get('upload_date')[4:6] + "-" + video.get('upload_date')[6:8]
 
-                  #time to add thumbnail
-                  update = {}
-                  update['id'] = scene.get('id')
-                  update['cover_image'] = video.get('thumbnail')
-                  stash.update_scene(update)
+    stash.update_scenes(scene_data)
+
+    #time to add thumbnail
+    update = {}
+    update['id'] = scene.get('id')
+    update['cover_image'] = video.get('thumbnail')
+    stash.update_scene(update)
 
 def read_urls_and_download(stash):
-    with open(config.urls_txt, 'r') as url_file:
-        urls = url_file.readlines()
+    if config.urls_type == "text":
+        with open(config.urls_file, 'r') as url_file:
+            urls = url_file.readlines()
+    elif config.urls_type == "json":
+        with open(config.urls_file, 'r') as url_file:
+            urls = json.load(url_file)
+    else:
+        log.error(f"You need to configure the config.py with your url filename and type")
+        sys.exit()
+
     with open(config.grabbed_urls_txt, 'r') as url_file:
         grabbed_urls = url_file.read()
-    downloaded = []
+    with open(config.error_urls_txt, 'r') as url_file:
+        error_urls = url_file.read()
+    
     total = len(urls)
+    if total > config.batchsize:
+       total = config.batchsize
     i = -1
+    
     for url in urls:
-        i += 1
-        log.progress(i/total)
-        this_url = url.strip()
+        if config.urls_type == "text":
+            this_url = url.strip()
+        elif config.urls_type == "json":
+            this_url = url.get(config.url_json_urlname)    
+
         if this_url in grabbed_urls:
             log.debug(f"Already grabbed {this_url}")
-        elif check_url_valid(this_url):
-            download(this_url, downloaded)
-            with open(config.grabbed_urls_txt, 'a') as url_file:
-                url_file.write(this_url+"\n")
-    if os.path.isfile(config.downloaded_json):
-        shutil.move(config.downloaded_json, config.downloaded_backup_json)
-    with open(config.downloaded_json, 'w') as outfile:
-        json.dump(downloaded, outfile)
+            continue
+        if this_url in error_urls:
+            log.debug(f"Already had an error with {this_url}")
+            continue
+        if not check_url_valid(this_url):
+            log.error(f"Bad URL: {this_url}")
+            with open(config.error_urls_txt, 'a') as url_file:
+                  url_file.write(this_url+"\n")
+            continue
+        i += 1
+        if i >= config.batchsize:
+            stash.metadata_scan(paths=[config.download_dir])
+            stash.run_plugin_task("ytdlp", "Download Videos", args={"mode": "download"})
+            return
+        log.progress(i/total)
+        fileid = download(this_url)
+        if fileid == 0:
+            log.error(f"Problem downloading {this_url}")
+            i += -1
+            continue
+    stash.metadata_scan(paths=[config.download_dir])
+    return
 
 def check_url_valid(url):
     regex = re.compile(
@@ -184,10 +204,10 @@ def check_url_valid(url):
 
     return re.match(regex, url) is not None
 
-def download(url, downloaded):
+def download(url):
     ytdl_options = config.ytdl_options
     download_dir = config.download_dir.rstrip("/") + "/"
-    log.debug("Downloading " + url + " to: " + download_dir)
+    log.info("Downloading " + url + " to: " + download_dir)
 
     ydl = yt_dlp.YoutubeDL({
         'outtmpl': download_dir + '%(id)s.%(ext)s',
@@ -195,15 +215,28 @@ def download(url, downloaded):
         **ytdl_options,
     })
 
+    meta = {}
     with ydl:
         try:
             info = ydl.extract_info(url=url, download=True)
             #ydl.sanitize_info makes the info json-serializable
             meta = ydl.sanitize_info(info)
-            log.debug(meta['id'])
-            log.debug("Download finished!")
-            downloaded.append(meta)
+            log.debug(f"File {meta.get('id')} - Download finished!")
         except Exception as e:
+            if "Error 404" in str(e) or "This video has been disabled" in str(e):
+               with open(config.error_urls_txt, 'a') as url_file:
+                  url_file.write(url+"\n")
+               return 0
             log.warning(str(e))
+
+    # save the metadata, so we can read it on create/scan
+    if meta.get('id') and not os.path.exists(download_dir + meta.get('id') + '.json'):
+       with open(download_dir + meta.get('id') + '.json', 'w') as outfile:
+          json.dump(meta, outfile)
+       with open(config.grabbed_urls_txt, 'a') as url_file:
+          url_file.write(url+"\n")
+       return meta.get('id')
+    log.debug(f"No ID found for this video, problem likely, so no json saved")
+    return 0
 
 main()
--- AcquisitionToolKit/plugins/YTDLP/ytdlp_defaults.py
+++ AcquisitionToolKit/plugins/YTDLP/ytdlp_defaults.py
@@ -1,18 +1,28 @@
 ## YTDLP defaults - DO NOT EDIT (could be overridden by later versions)
 ## Edit the config.py which copies this file on your first run (and stops)
-
+#
+# your editable config, uncomment anything you to set values on
+#
 # make sure your download directory is INSIDE your library
 download_dir = "/put your/download/directory/here/"
 
 # put your desired download urls into this file
-urls_txt = "urls.txt"
+urls_file = "urls.txt"
+
+#type of file it is:  text list or json?
+urls_type = "text"
+
+#if json, what fieldname holds the url?
+url_json_urlname = "videoUrl"
 
 # a history of downloads is here, to avoid redownloading, even if you move or rename the original download
 grabbed_urls_txt = "grabbed_urls.txt"
 
-# we store the json results here
-downloaded_json = "downloaded.json"
-downloaded_backup_json = "downloaded_backup.json"
+# a history of errors in downloads is here
+error_urls_txt = "error_urls.txt"
+
+# batch size, number of urls to fetch on one button push
+batchsize = 5
 
 # A list of options for use with the youtube_dl module
 # You can find available options here:
